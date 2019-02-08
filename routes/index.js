6b993ad6db44e7f16f358037a7498ac1d9476fa8
Huy Le <hql005@ucsd.edu>
2019-02-07 17:42:27 -0800

    recent

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@
 /*
  * GET home page.
  */
+express=require('express');
+router=express.Router();
 
-exports.view = function(req, res){
+
+router.get('/', function(req, res){
   res.render('index');
-};
+});
+
+router.get('/overview', function(req, res){
+  res.render('overview');
+});
